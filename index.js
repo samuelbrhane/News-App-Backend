c563b06c6a8f0f54d47af3435890ca66b22d5339
samuelberhane <1samibrhane@gmail.com>
2023-03-20 11:15:55 +0300

    Create express server for news app backend

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const morgan = require("morgan");
+const helmet = require("helmet");
+const dotenv = require("dotenv");
+
+// Configuration
+dotenv.config();
+const port = process.env.PORT || 8000;
+app.use(express.json());
+app.use(cors());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("tiny"));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Routes
+app.get("/", (req, res) => {
+  res.status(200).send("<h1>Welcome to News API.</h1>");
+});
+
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`Connect to DB & Server running on port ${port}`);
+    });
+  });
